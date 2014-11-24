0d4b84feed4967258a8364a55f3de1960560c73c
Bill Noble <winobes@gmail.com>
2014-11-24 14:59:59 +0100

    Separate vocabs.

--- model0.py
+++ model0.py
@@ -16,58 +16,45 @@ def em(F, alpha):
     # know what the indexes refer to in the end
     # TODO: if we end up using index arrays, some of this conversion
     # to indices should be done in the preprocessing stage.
-    vocab = dict()
-    V = 0 # size of the vocabulary
-    verbs = []; subjects = []; objects = []
-    counts_v = dict(); counts_s = dict(); counts_o = dict();
+    vocab = {'v': {}, 's': {}, 'o': {}}
+    counts = {'v': {}, 's': {}, 'o': {}}
+    V = {'v': 0, 's': 0, 'o': 0}  # size of the vocabulary
+    data = [] 
     with open("Preprocessing/test.concat") as f:
-        for v,s,o,c,_ in map(lambda x: x.split(' '), f.read().splitlines()):
-            if not v in vocab: vocab[v] = V; V += 1
-            if not s in vocab: vocab[s] = V; V += 1
-            if not o in vocab: vocab[o] = V; V += 1
-            verbs.append(vocab[v])
-            subjects.append(vocab[s])
-            objects.append(vocab[o])
-            c = int(c)
-            if vocab[v] in counts_v: counts_v[vocab[v]] += c
-            else: counts_v[vocab[v]] = c
-            if vocab[s] in counts_s: counts_s[vocab[s]] += c
-            else: counts_s[vocab[s]] = c
-            if vocab[o] in counts_o: counts_o[vocab[o]] += c
-            else: counts_o[vocab[o]] = c
-
-    for w in range(V):
-        if not w in counts_v: counts_v[w] = 0
-        if not w in counts_s: counts_s[w] = 0
-        if not w in counts_o: counts_o[w] = 0
+        for v,s,o,c in map(lambda x: x.split(' ')[:-1], f.read().splitlines()):
+            for (w, a) in zip((v,s,o), ('v','s','o')):
+                if not w in vocab[a]: 
+                    vocab[a][w] = V[a]
+                    V[a] += 1
+                if vocab[a][w] in counts[a]: 
+                    counts[a][vocab[a][w]] += int(c)
+                else:
+                    counts[a][vocab[a][w]] = int(c)
+            data.append((vocab['v'][v], vocab['s'][s], vocab['o'][o]))
 
     # make the index arrays
-    N = len(verbs)
-    verbs = np.array(verbs)
-    subjects = np.array(subjects)
-    objects = np.array(objects)
+    N = len(data)
+
+    data = np.array(data).T
 
     # initialize theta to the uniform distribution
     theta = np.ones(F) / F
 
     # randomly initialize the argument distributions for each frame
-    phi_v = np.random.dirichlet(np.ones(V) * alpha, F).T
-    phi_s = np.random.dirichlet(np.ones(V) * alpha, F).T
-    phi_o = np.random.dirichlet(np.ones(V) * alpha, F).T
+    phi_v = np.random.dirichlet(np.ones(V['v']) * alpha, F).T
+    phi_s = np.random.dirichlet(np.ones(V['s']) * alpha, F).T
+    phi_o = np.random.dirichlet(np.ones(V['o']) * alpha, F).T
 
     mu = np.zeros([N,F])
-    step = 0
     while True:
-        step+=1
-        print(step)
         # E-step
-        mu = phi_v[verbs] * phi_s[subjects] * phi_o[objects] * theta
+        mu = phi_v[data[0]] * phi_s[data[1]] * phi_o[data[2]] * theta
 
         # M-step
         theta_new = mu.sum(axis=0) 
-        phi_v_new = np.array([[mu.sum(axis=0)[f] * counts_v[w] for f in range(F)] for w in range(V)])
-        phi_s_new = np.array([[mu.sum(axis=0)[f] * counts_s[w] for f in range(F)] for w in range(V)])
-        phi_o_new = np.array([[mu.sum(axis=0)[f] * counts_o[w] for f in range(F)] for w in range(V)])
+        phi_v_new = np.array([[mu.sum(axis=0)[f] * counts['v'][w] for f in range(F)] for w in range(V['v'])])
+        phi_s_new = np.array([[mu.sum(axis=0)[f] * counts['s'][w] for f in range(F)] for w in range(V['s'])])
+        phi_o_new = np.array([[mu.sum(axis=0)[f] * counts['o'][w] for f in range(F)] for w in range(V['o'])])
 
         delta = (abs(np.subtract(phi_v, phi_v_new).sum()) +
                 abs(np.subtract(phi_s, phi_s_new).sum()) +
@@ -79,8 +66,8 @@ def em(F, alpha):
         phi_s = phi_s_new
         phi_o = phi_o_new
 
-        print(delta)
+        print("delta =", delta)
         if delta < 0.001: 
             return np.argmax(mu, axis=1)
 
-print(em(10, 1.5))
+print(em(10,1.5))
