9e850666470855a4354f2fd3238edfa5f15637b1
Bill Noble <winobes@gmail.com>
2014-11-24 15:46:50 +0100

    Vectorize phi calculations in M-step.

--- model0.py
+++ model0.py
@@ -17,19 +17,18 @@ def em(F, alpha):
     # TODO: if we end up using index arrays, some of this conversion
     # to indices should be done in the preprocessing stage.
     vocab = {'v': {}, 's': {}, 'o': {}}
-    counts = {'v': {}, 's': {}, 'o': {}}
+    counts = {'v': [], 's': [], 'o': []}
     V = {'v': 0, 's': 0, 'o': 0}  # size of the vocabulary
     data = [] 
-    with open("Preprocessing/test.concat") as f:
+    with open("Preprocessing/all_VSOs.sorted.concat") as f:
         for v,s,o,c in map(lambda x: x.split(' ')[:-1], f.read().splitlines()):
             for (w, a) in zip((v,s,o), ('v','s','o')):
                 if not w in vocab[a]: 
                     vocab[a][w] = V[a]
                     V[a] += 1
-                if vocab[a][w] in counts[a]: 
+                    counts[a].append(int(c))
+                else: 
                     counts[a][vocab[a][w]] += int(c)
-                else:
-                    counts[a][vocab[a][w]] = int(c)
             data.append((vocab['v'][v], vocab['s'][s], vocab['o'][o]))
 
     # make the index arrays
@@ -52,22 +51,21 @@ def em(F, alpha):
 
         # M-step
         theta_new = mu.sum(axis=0) 
-        phi_v_new = np.array([[mu.sum(axis=0)[f] * counts['v'][w] for f in range(F)] for w in range(V['v'])])
-        phi_s_new = np.array([[mu.sum(axis=0)[f] * counts['s'][w] for f in range(F)] for w in range(V['s'])])
-        phi_o_new = np.array([[mu.sum(axis=0)[f] * counts['o'][w] for f in range(F)] for w in range(V['o'])])
+        phi_v_new = np.outer(counts['v'], mu.sum(axis=0))
+        phi_s_new = np.outer(counts['s'], mu.sum(axis=0))
+        phi_o_new = np.outer(counts['o'], mu.sum(axis=0))
 
         delta = (abs(np.subtract(phi_v, phi_v_new).sum()) +
-                abs(np.subtract(phi_s, phi_s_new).sum()) +
-                abs(np.subtract(phi_o, phi_o_new).sum()) +
-                abs(np.subtract(theta, theta_new).sum()))
+                 abs(np.subtract(phi_s, phi_s_new).sum()) +
+                 abs(np.subtract(phi_o, phi_o_new).sum()) +
+                 abs(np.subtract(theta, theta_new).sum()))
 
         theta = theta_new
         phi_v = phi_v_new
         phi_s = phi_s_new
         phi_o = phi_o_new
 
-        print("delta =", delta)
         if delta < 0.001: 
             return np.argmax(mu, axis=1)
 
-print(em(10,1.5))
+print(em(100,1.5))
