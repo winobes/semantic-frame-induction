5deccc292dcbf5da9509836d48118e34461b5a6a
jacob5677688 <jacob.verdegaal@student.uva.nl>
2014-12-03 21:21:25 +0100

    added plotting

--- evaluation.py
+++ evaluation.py
@@ -1,24 +1,31 @@
 import numpy as np
 import model0 as mod0
 import random as rnd
+import matplotlib.pyplot as pp
 
 def frame_coherency(K):
-    coh = 0
-    (frame_dists, frame_assign, theta, data) = mod0.em(10,1.5) 
-    probs = { }
-    for i in range(K):
-        ((v,s,o),f) = frame_assign.popitem()
-        print(v,s,o,f)
-        probs[(v,s,o)] = frame_dists[f]['v'][v]*frame_dists[f]['s'][s]*frame_dists[f]['o'][o]*theta[f]
 
-    N = len(data)
-    for tup in probs:
-        vR,_,_ = data[rnd.randint(0,N)]
-        probR = np.argmax([frame_dists[frm]['v'][vR]*frame_dists[frm]['s'][s]*frame_dists[frm]['o'][o]*theta[frm] for frm in frame_dists])
-        if probs[tup] > probR:
-            coh +=1
+    results = []
+    frmsL = [5*(n+1) for n in range(10)]
+    for frms in frmsL:
+        coh = 0
+        (frame_dists, frame_assign, theta, data) = mod0.em(frms,1.5) 
+        N = len(data)
+        probs = { }
+        for i in range(K):
+            ((v,s,o),f) = frame_assign.popitem()
+            print(v,s,o,f)
+            probs[(v,s,o)] = frame_dists[f]['v'][v]*frame_dists[f]['s'][s]*frame_dists[f]['o'][o]*theta[f]
 
-    print(" accuracy:", coh/K)
-   
+        for tup in probs:
+            vR,_,_ = data[rnd.randint(0,N)]
+            probR = np.argmax([frame_dists[frm]['v'][vR]*frame_dists[frm]['s'][s]*frame_dists[frm]['o'][o]*theta[frm] for frm in frame_dists])
+            if probs[tup] > probR:
+                coh +=1
 
-frame_coherency(1000)
+        results.append(coh/K)
+
+    pp.plot(frmsL,results)
+    pp.show()
+
+frame_coherency(1000 )
