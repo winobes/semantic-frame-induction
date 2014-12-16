e47d5cec5b43b7198a1877b1d1c416e4cae9bd19
Bill Noble <winobes@gmail.com>
2014-12-16 14:28:39 +0100

    cleaning up

---
+++ probfuncs.py
@@ -0,0 +1,37 @@
+from itertools import accumulate
+import random
+import bisect
+from collections import Sequence
+
+def normalize(dist):
+    min_val = min(dist) 
+    if min_val < 0:
+        dist = [x - min_val for x in dist]
+        #dist -= min_val 
+    normalizer = sum(dist)
+    return [x/normalizer for x in dist]
+
+def cumulative(dist):
+    return list(accumulate(dist))
+
+def cum_dist_choice(dist):
+    val = random.random()
+    for i in range(len(dist)):
+        if val < dist[i]:
+            return i
+
+# http://stackoverflow.com/a/13052108/2224317
+def weighted_sample(population, weights, k):
+    return random.sample(WeightedPopulation(population, weights), k)
+
+class WeightedPopulation(Sequence):
+    def __init__(self, population, weights):
+        assert len(population) == len(weights) > 0
+        self.population = population
+        self.cumweights = cumulative(weights)
+    def __len__(self):
+        return self.cumweights[-1]
+    def __getitem__(self, i):
+        if not 0 <= i < len(self):
+            raise IndexError(i)
+        return self.population[bisect.bisect(self.cumweights, i)]
--- semanticFrameInduction.py
+++ semanticFrameInduction.py
@@ -9,8 +9,8 @@ from probfuncs import weighted_sample
 
 args = ('v','s','o')
 
-def pruneData( dataFile='Preprocessing/all_VSOs.sorted.concat', verbPC=20):
-    print('Pruning to %', verbPC, '... ', end='')
+def pruneData( dataFile='Preprocessing/all_VSOs.sorted.concat', cutoffPC=20):
+    print('Pruning to %', cutoffPC, '... ', end='')
     V = {}
     data = {}
     with open(dataFile) as f:
@@ -29,8 +29,8 @@ def pruneData( dataFile='Preprocessing/all_VSOs.sorted.concat', verbPC=20):
     # determine the cutoff 
     counts = list(V.values())
     counts.sort(reverse=True)
-    cutoff = counts[int(len(counts) * verbPC/100)]
-    print("The cutoff for verb frequency will be", cutoff)
+    cutoff = counts[int(len(counts) * cutoffPC/100)]
+    print("The absolute cutoff for verb frequency will be", cutoff)
     # pop off the data with low counts
     for (v,s,o) in data.copy():
         if V[v] < cutoff:
@@ -39,76 +39,57 @@ def pruneData( dataFile='Preprocessing/all_VSOs.sorted.concat', verbPC=20):
     pickle.dump(data, open("allData.pkl", 'wb'))
     print('Done.')
 
-def splitData( dataFile='allData.pkl', testPC=10):
-
-    print('Splitting off %', testPC, ' for training data...')
+def splitData( dataFile='allData.pkl', testPC=10, xvPC=20):
 
     # load up all the data
     if not os.path.isfile(dataFile):
         raise ValueError ("must run pruneData first.")
     data = pickle.load(open(dataFile, 'rb'))
+    trainingData = data.copy()
 
     # make a weighted sample of data
     dataList = [s for s in data]
     weights = [data[s] for s in dataList]
-    testSize = int(len(data) * testPC/100) 
-    testDataList = weighted_sample(dataList, weights, testSize)
 
-    # put the sample in a good format and adjust the training data counts
-    testData = {}
-    while testDataList:
-        vso = testDataList.pop()
-        data[vso] -= 1
-        if vso in testData:
-            testData[vso] += 1
-            if data[vso] == 0:
-                data.pop(vso)
-        else: 
-            testData[vso] = 1
+    # genenic function for generating the samples from w/o replacement
+    def sample_from_data(PC): 
+        size = int(len(data) * PC/100) 
+        sampleList = weighted_sample(dataList, weights, size)
 
+        # put the sample in a good format and adjust the data counts
+        sampleData = {}
+        while sampleList:
+            vso = sampleList.pop()
+            trainingData[vso] -= 1
+            if vso in sampleData: 
+                sampleData[vso] += 1
+                if trainingData[vso] == 0:
+                    trainingData.pop(vso)
+            else: 
+                sampleData[vso] = 1
+        return sampleData
+
+    # generate both samples
+    print('Splitting off %', testPC, ' for training data...')
+    testData = sample_from_data(testPC)
+    print('Splitting off %', xvPC, ' for cross verb data...')
+    xvData   = sample_from_data(xvPC)
+
+    # check that we didn't make any mistakes...
+    def merge_counts(d1, d2):
+        d3 = d1.copy()
+        for val in d2:
+            if val in d1:
+                d3[val] += d2[val]
+            else:
+                d3[val] = d2[val]
+        return d3
+    assert data == merge_counts(trainingData, merge_counts(testData, xvData))
+
+    # dump it!
     pickle.dump(data, open("trainingData.pkl", 'wb'))
     pickle.dump(testData, open("testingData.pkl", 'wb'))
-
-
-def loadData( dataFile , trainPC, xvalidPC, testPC):
-
-    counts = []
-    word_to_index = {a: {} for a in args}
-    index_to_word = {a: {} for a in args}
-    V = {a: 0 for a in args}  
-    trnData = {a: [] for a in args}
-
-    xvData = []
-    tstData= []
-
-    nD = 0
-    nTrn = 0
-    nXval = 0
-    nTst = 0
-    
-
-    
-    with open( dataFile) as f:
-        for v,s,o,c in map(lambda x: x.split(' ')[:-1], f.read().splitlines()):
-            c = int(c)
-            if c > 10:
-                nD += 1 
-                splt = c*(trainPC/100)
-                counts.append(int(splt))
-                for (w, a) in zip((v,s,o), args):
-                    if not w in word_to_index[a]: 
-                        word_to_index[a][w] = V[a]
-                        index_to_word[a][V[a]] = w
-                        V[a] += 1
-                    trnData[a].append(word_to_index[a][w])
-                splt2 = int((c-splt)* (xvalidPC/100)) 
-                xvData.append((v,s,o, splt2))
-                tstData.append((v,s,o, c-splt-splt2))
-
-    #print("\n Data lengths: train, xval, test: ", len(trnData['v']), len(xvData), len(tstData))
-                                        
-    return ((counts, word_to_index, index_to_word, V, trnData), xvData, tstData)
-    
+    pickle.dump(xvData, open("xvData.pkl", 'wb'))
 
 def runTests(*params):
     
@@ -121,7 +102,9 @@ def runTests(*params):
 
     print("\n ==================================\n Running tests with: \n frames:\t", frames, "\n alpha's:\t ", alphas, "\n")
 
-    trnData, xvData, tstData = loadData("all_VSOs.sorted.concat",70,20,10)
+    trnData = pickle.load(open('trainingData.pkl', 'rb'))
+    tstData = pickle.load(open('testingData.pkl', 'rb'))
+    xvData  = pickle.load(open('xvData.pkl', 'rb'))
     #print("\n Data loaded \n")
     
     results = {(f,a): [] for f in frames for a in alphas}
@@ -132,7 +115,7 @@ def runTests(*params):
     for a in alphas:
         for f in frames:
             print( "\n training with: f=%d , a=%f" %(f,a)  )
-            model = mod0.em(f,a,*trnData)
+            model = mod0.em(f,a, trnData)
             print("\n testing...")
             res = evalu.frame_coherency(model, xvData)
             #results[(f,a)] = [model, res]
@@ -195,9 +178,7 @@ def get_result_table():
     return results 
     
 
+#pruneData()
+#splitData()
 #runTests()
 
-pruneData()
-splitData()
-
-#mod0.em(20, 1.5, pickle.load(open('trainingData.pkl', 'rb')))
